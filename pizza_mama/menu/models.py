e6ae28962d2bf56f66e3cc8f054dfb5c1aadb7b6
steeve0403 <zychsteeve4@gmail.com>
2024-08-29 12:29:45 +0200

    start models Pizza

--- pizza_mama/menu/models.py
+++ pizza_mama/menu/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Pizza(models.Model):
+    name = models.CharField(max_length=200)
+    ingredients = models.CharField(max_length=200)
+    price = models.FloatField(default=0)
+    vegetarian = models.BooleanField(default=False)
